98d284d6a283b8c8cbf4abcfce25ca1440e44cf2
JsonYaoo <1484073063@qq.com>
2020-12-29 23:16:06 +0800

    spring-cloud-demo> Eureka Server&Provider

---
+++ eureka/eureka-client/src/main/java/contoller/Controller.java
@@ -0,0 +1,30 @@
+package contoller;
+
+import com.imooc.springcloud.entity.Friend;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class Controller {
+
+    @Value("${server.post}")
+    private String port;
+
+    @GetMapping("/sayHi")
+    public String sayHi(){
+        return "Hello world~~~ " + port;
+    }
+
+    @PostMapping("/sayHiPost")
+    public Friend sayHiPost(@RequestBody Friend friend){
+        log.info("You are " + friend.getName());
+        friend.setPort(port);
+        return friend;
+    }
+
+}
---
+++ eureka/eureka-server/src/main/java/com/immoc/springcloud/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.immoc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(EurekaServerApplication.class);
+        new SpringApplicationBuilder(EurekaServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+
+}
