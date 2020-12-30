394bd3146f31c77b1b9d02ab9f9e0fcf29345565
yaocs2 <yaocs2@midea.com>
2020-12-30 10:45:03 +0800

    spring-cloud-demo> Feign-Client-intf初始化

---
+++ feign/feign-client-intf/src/main/java/com/immoc/springcloud/entity/Friend.java
@@ -0,0 +1,12 @@
+package com.immoc.springcloud.entity;
+
+import lombok.Data;
+
+@Data
+public class Friend {
+
+    private String name;
+
+    private String port;
+
+}
---
+++ feign/feign-client-intf/src/main/java/com/immoc/springcloud/service/IService.java
@@ -0,0 +1,25 @@
+package com.immoc.springcloud.service;
+
+import com.immoc.springcloud.entity.Friend;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("feign-client")
+public interface IService {
+
+    @GetMapping("/sayHi")
+    public String sayHi();
+
+    @PostMapping("/sayHi")
+    public String sayHiPost(@RequestBody Friend friend);
+
+    @GetMapping("/retry")
+    public String retry(@RequestParam(name = "timeout") int timeout);
+
+    @GetMapping("/error")
+    public String error();
+
+}
